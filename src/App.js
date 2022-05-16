fa264a9d4605eac2eb5752f76672939fa00facc4
RDjarbeng <richarddjarbeng@gmail.com>
2022-05-16 15:58:08 +0000

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import * as toxicity from '@tensorflow-models/toxicity';
+import '@tensorflow/tfjs';
 
 export default function App() {
+  const [model, setModel] = useState();
+
+  useEffect(() => {
+    const loadModel = async () => {
+      console.log('Model loading...');
+
+      const threshold = 0.9;
+      const toxicityModel = await toxicity.load(threshold);
+      setModel(toxicityModel);
+
+      console.log('Model loaded');
+    };
+
+    loadModel();
+  }, []);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
